780e5e2c7e587d91eefc308346098ac920651c04
Jerell Alexander <jerellalexander15@gmail.com>
2024-11-26 00:36:44 -0400

    modified:   App/models/sentimentCommand.py

--- App/models/sentimentCommand.py
+++ App/models/sentimentCommand.py
@@ -1,18 +1,7 @@
-from App.database import db
+from abc import ABC, abstractmethod
 
-class SentimentCommand(db.Model):
-    __tablename__ = 'sentiment_command'
-    id = db.Column(db.Integer, primary_key=True)
-    command = db.Column(db.String(50))
-
-
-    def __init__(self, command):
-        self.id = id
-        self.command = command
-
-    def get_json(self):
-        return{
-            'id': self.id,
-            'command': self.command,
-        }
-    
+class SentimentCommand(ABC):
+    @abstractmethod
+    def execute(self):
+        """Execute the sentiment command."""
+        pass
