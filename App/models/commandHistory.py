271c06efe2a4197ddfe7b8a36b5161b085eee0b9
HonchoTheConquistador <jehlani.joseph@my.uwi.edu>
2024-11-26 04:41:04 -0400

    Reworked the commandHistory class,  will have to edit the model diagram to suit

--- App/models/commandHistory.py
+++ App/models/commandHistory.py
@@ -7,12 +7,11 @@ from datetime import datetime
 class CommandHistory(db.Model):
     __tablename__ = 'command_history'
     id = db.Column(db.Integer, primary_key=True)
-    history = db.relationship('SentimentCommand', backref='command_history', lazy=True)
+    review_id = db.Column(db.Integer, db.ForeignKey('review.ID'), nullable=False)
     time = db.Column(db.DateTime, nullable=False)
 
-
-    def __init__(self, history: SentimentCommand):
-        self.history = history
+    def __init__(self, review_id):
+        self.review_id = review_id
         self.time = datetime.now()
 
     def get_json(self):
