610497f3c0b68900db06d61fe039050140c5b605
HonchoTheConquistador <jehlani.joseph@my.uwi.edu>
2024-11-25 22:38:18 -0400

    Updated invoker

---
+++ App/models/StudentSentimentInvoker.py
@@ -0,0 +1,18 @@
+from App.database import db
+from App.models import SentimentCommand 
+
+
+class StudentSentimentInvoker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self):
+        self.command = None 
+
+    def setCommand(self, command: SentimentCommand):
+        self.command = command
+
+    def execute(self):
+        if self.command is not None:
+            self.command.execute()
+        else:
+            print("No command set")
