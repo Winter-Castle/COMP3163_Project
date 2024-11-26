a581b84722571cc29d06a7438176e3cec7c860aa
HonchoTheConquistador <jehlani.joseph@my.uwi.edu>
2024-11-26 04:26:57 -0400

    put some logic in the class

--- App/models/commandHistory.py
+++ App/models/commandHistory.py
@@ -1,15 +1,19 @@
 from App.database import db
+from App.models.sentimentCommand import SentimentCommand
+
+from datetime import datetime
+
 
 class CommandHistory(db.Model):
     __tablename__ = 'command_history'
     id = db.Column(db.Integer, primary_key=True)
     history = db.relationship('SentimentCommand', backref='command_history', lazy=True)
+    time = db.Column(db.DateTime, nullable=False)
 
 
-
-    def __init__(self, history):
-        self.id = id
+    def __init__(self, history: SentimentCommand):
         self.history = history
+        self.time = datetime.now()
 
     def get_json(self):
         return{
